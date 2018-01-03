ce2cca556e65d728178a3f8cc8aad0384fbc9d34
aoi-lucario <petersbw123@gmail.com>
2018-01-03 17:30:46 +0900

    refactoring trainer (/acc_recog)

--- acc_recog/trainer.py
+++ acc_recog/trainer.py
@@ -9,10 +9,14 @@ from chainer import using_config, no_backprop_mode, Variable, optimizers, serial
 import chainer.functions as F
 import model
 
-ROOT = './'
 
 #################
 ## init
+
+ROOT = './'
+xp = cuda.cupy
+# xp = np   ### use CPU
+
 logging.basicConfig(level=logging.DEBUG,
                     format='[%(levelname)s]   \t %(asctime)s \t%(message)s\t',
                     datefmt='%Y/%m/%d (%A) - %H:%M:%S',
@@ -26,115 +30,108 @@ formatter = logging.Formatter('[%(levelname)s]  \t%(message)s\t')
 console.setFormatter(formatter)
 logging.getLogger('').addHandler(console)
 
-#################
-### read dataset
-logging.info('reading dataset')
+class Model(Object):
+    def __init__(self):
+        logging.info('reading dataset')
+        dataset = np.load(ROOT + 'spec_dataset_e0.npy')
+        dataflag = np.load(ROOT + 'dataflag.npy')[:, 1]
+        print("dataset.shape:", dataset.shape)
+        length = len(dataset)
+        gap = int(length*0.2)  # num of test samples
+        ran_list = np.random.permutation(length)
+        self.x_train = dataset[ran_list[gap:],]
+        self.y_train = dataflag[ran_list[gap:],]
 
-dataset = np.load(ROOT + 'spec_dataset_e0.npy')
-dataflag = np.load(ROOT + 'dataflag.npy')[:, 1]
+        self.x_test = xp.array(dataset[ran_list[:gap],])
+        self.y_test = xp.array(dataflag[ran_list[:gap],])
+        self.V_x_test = Variable(self.x_test)
 
-print("dataset.shape:", dataset.shape)
+        # x_train = dataset[ran_list,]  # test-data included
+        # y_train = dataflag[ran_list,]  # test-data included
 
-length = len(dataset)
-gap = int(length*0.2)  # num of test samples
+        ### confirm
+        print("x_train.shape: ", self.x_train.shape)
+        print("x_test.shape: ", self.x_test.shape)
+        print("y_train.shape: ", self.y_train.shape)
+        print("y_test.shape: ", self.y_test.shape)
 
-ran_list = np.random.permutation(length)
-x_train = dataset[ran_list[gap:],]
-# x_train = dataset[ran_list,]  # test-data included
-x_test = dataset[ran_list[:gap],]
+        logging.info('setting model')
 
-y_train = dataflag[ran_list[gap:],]
-# y_train = dataflag[ran_list,]  # test-data included
-y_test = dataflag[ran_list[:gap],]
+        self.model = model.TINY_D()  # input: x, output: one hot y
+        self.lr = 0.02  # learning rate
+        self.optimizer = optimizers.Adam(alpha=self.lr)
+        self.optimizer.setup(model)
 
-#################
-## set model and optimizer
-logging.info('setting model')
+        ### to GPU
+        cuda.get_device_from_id(0).use()
+        self.model.to_gpu(0)
 
-model = model.TINY_D()  # input: x, output: one hot y
-optimizer = optimizers.Adam()
-optimizer.setup(model)
+        logging.info('GPU used')
 
-cuda.get_device_from_id(0).use()
-model.to_gpu(0)
-xp = cuda.cupy
+    def save_model(self, num):
+        logging.info('save model at epoch: %s' % num)
+        serializers.save_npz('cpu_model_%s.npz' % num, self.model)
+        logging.info('Model \'cpu_model_%s.npz\' Saved' % num)
 
-x_test = xp.array(x_test)
-y_test = xp.array(y_test)
+    def train(self, epoch=3000, bc=32):
+        n = len(self.x_train)
+        logging.info('training start')
+        for j in range(epoch):
+            # train
+            loss_sum = 0
+            sff_index = np.random.permutation(n)
+            for i in range(0, n, bc):
+                x = Variable(xp.array(self.x_train[sff_index[i: (i + bc) if (i + bc) < n else n], ]))
+                y = Variable(xp.array(self.y_train[sff_index[i: (i + bc) if (i + bc) < n else n], ]))
+                self.model.cleargrads()
+                loss = F.softmax_cross_entropy(self.model(x), y)
+                loss.backward()
+                self.optimizer.update()
+                loss_sum += loss.data
+            logging.debug("loop: %d, loss = %f" % (j, loss_sum))
 
-logging.info('GPU used')
+            if j % 10 == 0:
+                self.evaluated(self.model)
 
-#################
-## train
-print("x_train.shape: ", x_train.shape)
-print("x_test.shape: ", x_test.shape)
-print("y_train.shape: ", y_train.shape)
-print("y_test.shape: ", y_test.shape)
+            # update learning rate
+            if j%100 == 0:
+                rate = round(self.lr * ((epoch-j)/epoch), 4)
+                if not rate:
+                    rate = 0.000001
+                self.optimizer.alpha = rate
+                logging.debug("change alpha to %f" % rate)
 
-epoch = 3000
-n = len(x_train)
-bc = 32
+            if j%500 == 0:
+                save_model(str(j))
 
-def save_model(model, num):
-    serializers.save_npz('cpu_model_%s.npz' % num, model)
-    logging.info('Model \'cpu_model_%s.npz\' Saved' % num)
-
-logging.info('training start')
-for j in range(epoch):
-    # train
-    loss_sum = 0
-    sff_index = np.random.permutation(n)
-    for i in range(0, n, bc):
-        x = Variable(xp.array(x_train[sff_index[i: (i + bc) if (i + bc) < n else n], ]))
-        y = Variable(xp.array(y_train[sff_index[i: (i + bc) if (i + bc) < n else n], ]))
-        model.cleargrads()
-        loss = F.softmax_cross_entropy(model(x), y)
-        loss.backward()
-        optimizer.update()
-        loss_sum += loss.data
-    logging.debug("loop: %d, loss = %f" % (j, loss_sum))
-    # update learning rate
-    # optimizer.lr = learning_rate * (1 - batch / max_batches) ** lr_decay_power
-
-    # evaluated
-    if j % 10 == 0:
+    # return oringal output (need softmax to probability)
+    def predict(self, Vx=self.V_x_test):
         with no_backprop_mode():
-            xt = Variable(x_test)
             with using_config('train', False):
-                yt = model(xt)
-        loss_test = F.softmax_cross_entropy(yt, y_test).data
+                return self.model(Vx)
+
+    def evaluated(self, true_yt=self.y_test):
+        yt = self.predict()
+        loss_test = F.softmax_cross_entropy(yt, true_yt).data
         logging.info("test: loss = %f" % loss_test)
         ans = yt.data
         nrow, ncol = ans.shape
         acc = 0
         """
-        # print probability
-        anss = F.softmax(ans)
+        ### print probability
+        Sans = F.softmax(ans)
         for i in range(nrow):
-            print("ans[%d]: " % i, anss[i])
+            print("ans[%d]: " % i, Sans[i])
         """
         for i in range(nrow):
             cls = int(np.argmax(ans[i]))  # one hot -> int
             if cls == y_test[i]:
                 acc += 1
-        print("accurate: %d/%d = %f" % (acc, y_test.shape[0], acc/y_test.shape[0]))
+        print("accurate: %d/%d = %f" % (acc, self.y_test.shape[0], acc/self.y_test.shape[0]))
 
-        if j%100 == 0:
-            rate = round(0.001*((epoch-j)/epoch), 6)
-            if not rate:
-                rate = 0.000001
-            optimizer.alpha = rate
-            logging.debug("change alpha to %f" % rate)
 
-        if j%500 == 0:
-            logging.info('save model at epoch: %d' % j)
-            save_model(model, str(j))
-
-logging.info('training end')
-save_model(model, 'end')
-
-#################
-## test
-
-#################
-## evaluate
+if __name__ == '__main__':
+    M = Model()
+    M.train()
+    M.save_model('end')
+    logging.info('training end')
