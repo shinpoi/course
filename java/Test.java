adcb264131ce3ce46a00615f11b716d248f09fc5
aoi-lucario <petersbw123@gmail.com>
2018-04-15 12:11:30 +0900

    update ./java

--- java/Test.java
+++ java/Test.java
@@ -1,5 +1,6 @@
 import java.util.Date;
 import java.util.Arrays;
+import java.io.*;
 
 public class Test{
 	public static void main(String args[]){
@@ -23,5 +24,44 @@ public class Test{
 		Date date = new Date();
 		System.out.println(date);
 		System.out.println(date.getTime());
+
+		char c=0;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Input char, \"q\" to esc.");
+		do{
+			try{c = (char)br.read();}
+			catch(IOException err){System.out.println("get IOErr as "+err);}
+			System.out.println(c+"="+(int)c);
+		}while(c!=113&&c!=81);
+		
+		String str="";
+		System.out.println("Input String, \"esc\" to esc.");
+		do{
+			try{str = br.readLine();}
+			catch(IOException err){System.out.println("get IOErr as "+err);}
+			System.out.println("str = "+str);
+		}while(!str.equals("esc"));
+		
+		int ch=0;
+		try{
+			InputStream f = new FileInputStream("/home/aoi-lucario/bashrc");
+			try{
+				InputStreamReader ir = new InputStreamReader(f, "utf-8");
+				try{
+					while((ch=ir.read())!=-1){
+						System.out.write((char)ch);
+					}
+				}catch(IOException err){
+					System.out.println("IOErr: "+err);
+					System.exit(-1);
+					}
+			}catch(UnsupportedEncodingException err){
+					System.out.println("EncodeErr: "+err);
+					System.exit(-1);
+					}
+		}catch(FileNotFoundException err){
+				System.out.println("FileErr: "+err);
+				System.exit(-1);
+				}
 	}
 }
