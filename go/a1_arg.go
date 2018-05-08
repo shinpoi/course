2f32fe31b8a3e3f5d19d27a45cdde5990f74f60a
shinpoi <petersbw123@gmail.com>
2018-05-09 00:10:52 +0900

    add go/a3

--- go/a1_arg.go
+++ go/a1_arg.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
 
 func main(){
-	var s, sep string
-	for i:=1; i<len(os.Args);i++{
+	var s, sstd, sep string
+	
+	// Parameter
+	sep = " "
+	s += "args: "
+	/*for i:=2; i<len(os.Args);i++{
 		s += sep + os.Args[i]
-		sep = " "
+	}*/
+	for _, arg := range os.Args[1:]{
+		s += sep + arg
 	}
 	fmt.Println(s)
+	
+	// Stdin
+	sep = "\n"
+	sstd += "stdin: "
+	scanStdin := bufio.NewScanner(os.Stdin)
+	for scanStdin.Scan() {
+		sstd += scanStdin.Text() + sep
+	}
+	fmt.Println(sstd)
 }
---
+++ go/a2_dup.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	countsMap := make(map[string]int)
+	inputScan := bufio.NewScanner(os.Stdin)
+	for inputScan.Scan() {
+		countsMap[inputScan.Text()]++
+	}
+
+	for key, value := range countsMap {
+		if value > 1 {
+			fmt.Printf("%d\t%s\n",value, key)
+		}
+	}
+}
