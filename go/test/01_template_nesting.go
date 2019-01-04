c6bd9910880fbe7581cf6fca47e3b8be3c805366
Aoi-lucario <petersbw123@gmail.com>
2019-01-04 15:14:10 +0800

    fix test code

--- go/test/01_template_nesting.go
+++ go/test/01_template_nesting.go
@@ -1,29 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"text/template"
 )
 
-var tmpStr = `
-{{ block "c1" .}} default c1 {{ end }}
-`
+var tmpStr = `{{ block "c1" .}} default c1 {{ end }}`
 
-var c1 = `< {{ block "c2" .}} default c2 {{ end }} >`
-var c2 = `
-{{ define "c2" }}
-< {{ block "c3" .}} default c3 {{ end }} >
-{{ end }}
-`
-
-var c31 = `
-{{ define "c3" }}< this is block c3-1 >{{ end }}
-`
-
-var c32 = `
-{{ define "c3" }}< this is block c3-2 >{{ end }}
-`
+var c1 = `{{ define "c1" }}< {{ block "c2" .}} default c2 {{ end }} >{{ end }}`
+var c2 = `{{ define "c2" }}< {{ block "c3" .}} default c3 {{ end }} >{{ end }}`
+var c31 = `{{ define "c3" }}< this is block c3-1 >{{ end }}`
+var c32 = `{{ define "c3" }}< this is block c3-2 >{{ end }}`
 
 func main() {
 	tmpl, err := template.New("test_template").Parse(tmpStr)
@@ -31,7 +20,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	log.Println("\n\n---- define 1 ----")
+	fmt.Println("\n\n---- define 1 ----")
 	tmpl1, _ := tmpl.Clone()
 	tmpl1.Parse(c32)
 	tmpl1.Parse(c31)
@@ -39,7 +28,7 @@ func main() {
 	tmpl1.Parse(c1)
 	tmpl1.Execute(os.Stdout, nil)
 
-	log.Println("\n\n---- define 2 ----")
+	fmt.Println("\n\n---- define 2 ----")
 	tmpl2, _ := tmpl.Clone()
 	tmpl2.Parse(c1)
 	tmpl2.Parse(c2)
@@ -47,7 +36,7 @@ func main() {
 	tmpl2.Parse(c32)
 	tmpl2.Execute(os.Stdout, nil)
 
-	log.Println("\n\n---- define 2 ----")
+	fmt.Println("\n\n---- define 3 ----")
 	tmpl3, _ := tmpl.Clone()
 	tmpl3.Parse(c1)
 	tmpl3.Parse(c2)
